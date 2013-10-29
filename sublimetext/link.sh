59a3d53777738c47b99e5f560b326f4595940507
Daryl Lau <daryl.lau@hotmail.com>
2013-10-28 22:53:49 -0700

    add st3 keymap

--- sublimetext/link.sh
+++ sublimetext/link.sh
@@ -3,3 +3,5 @@ ABSPATH=$(cd "$(dirname "$0")"; pwd|sed 's/ /\\ /g')
 cd  ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
 ln -fs "$ABSPATH/Package Control.sublime-settings" .
 ln -fs "$ABSPATH/Preferences.sublime-settings" .
+ln -fs "$ABSPATH/Default\ (OSX)sublime-keymap" .
+
